f7c59841ebae48ab9108618707c60ac274a0f7cb
Tiago Caceraghi dos Santos <tiago.caceraghi@edu.sc.senai.br>
2022-08-02 19:59:29 -0300

    Versão 0.0.3 - Criação da View

--- src/main/java/br/com/senai/aula1907/servico/CategoriaServicoCRUD.java
+++ src/main/java/br/com/senai/aula1907/servico/CategoriaServicoCRUD.java
@@ -3,10 +3,12 @@ package br.com.senai.aula1907.servico;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.senai.aula1907.orm.Categoria;
 import br.com.senai.aula1907.repository.CategoriaRepository;
 
+@Service
 public class CategoriaServicoCRUD implements CategoriaServico{
 	
 	@Autowired
